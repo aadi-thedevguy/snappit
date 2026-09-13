30cca7fbf77586c5bad6fe873820324e4524458b
Aditya khare <adikhare24@gmail.com>
2026-09-13 22:51:42 +0530

    fix(app): updated inngest max duration value

--- app/api/inngest/route.ts
+++ app/api/inngest/route.ts
@@ -1,7 +1,7 @@
 import { serve } from "inngest/next";
 
 export const runtime = "nodejs";
-export const maxDuration = 800;
+export const maxDuration = 200;
 
 import { inngest } from "@/lib/inngest/client";
 import { transcodeVideoToMp4 } from "@/lib/inngest/functions/transcodeVideo";
